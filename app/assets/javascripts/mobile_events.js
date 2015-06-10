68cebd7d423e6c8a5de3c5f002407366ca10743a
Jack3000 <doom_bubba@walla.com>
2015-06-10 20:07:30 +0300

    swipe test twelve

--- app/assets/javascripts/mobile_events.js
+++ app/assets/javascripts/mobile_events.js
@@ -562,7 +562,7 @@ $.event.special.swipe = {
 	durationThreshold: 1000,
 	
 	// Swipe horizontal displacement must be more than this.
-	horizontalDistanceThreshold: 75,
+	horizontalDistanceThreshold: 30,
 	
 	// Swipe vertical displacement must be less than this.
 	verticalDistanceThreshold: 75,  
@@ -611,7 +611,7 @@ $.event.special.swipe = {
 								Math.abs( start.coords[ 1 ] - stop.coords[ 1 ] ) < $.event.special.swipe.verticalDistanceThreshold ) {
  
 							start.origin.trigger( "swipe" )
-								.trigger( Math.abs( start.coords[ 0 ] - stop.coords[ 0 ] ) > Math.abs( start.coords[ 1 ] - stop.coords[ 1 ] ) ? (start.coords[0] > stop.coords[ 0 ] ? "swipeleft" : "swiperight") : (start.coords[1] > stop.coords[ 1 ] ? "swipeup" : "swipedown") );
+								.trigger( start.coords[0] > stop.coords[ 0 ] ? "swipeleft" : "swiperight" );
 						}
 					}
 					start = stop = undefined;
@@ -624,9 +624,7 @@ $.event.special.swipe = {
 $.each({
 	taphold: "tap",
 	swipeleft: "swipe",
-	swiperight: "swipe",
-	swipeup: "swipe",
-	swipedown: "swipe"
+	swiperight: "swipe"
 }, function( event, sourceEvent ) {
 	$.event.special[ event ] = {
 		setup: function() {
@@ -636,3 +634,79 @@ $.each({
 });
  
 })( jQuery, this );
+(function () {
+// initializes touch and scroll events
+    var supportTouch = $.support.touch,
+        scrollEvent = "touchmove scroll",
+        touchStartEvent = supportTouch ? "touchstart" : "mousedown",
+        touchStopEvent = supportTouch ? "touchend" : "mouseup",
+        touchMoveEvent = supportTouch ? "touchmove" : "mousemove";
+ 
+    // handles swipe up and swipe down
+    $.event.special.swipeupdown = {
+        setup: function () {
+            var thisObject = this;
+            var $this = $(thisObject);
+ 
+            $this.bind(touchStartEvent, function (event) {
+                var data = event.originalEvent.touches ?
+                        event.originalEvent.touches[ 0 ] :
+                        event,
+                    start = {
+                        time: (new Date).getTime(),
+                        coords: [ data.pageX, data.pageY ],
+                        origin: $(event.target)
+                    },
+                    stop;
+ 
+                function moveHandler(event) {
+                    if (!start) {
+                        return;
+                    }
+ 
+                    var data = event.originalEvent.touches ?
+                        event.originalEvent.touches[ 0 ] :
+                        event;
+                    stop = {
+                        time: (new Date).getTime(),
+                        coords: [ data.pageX, data.pageY ]
+                    };
+ 
+                    // prevent scrolling
+                    if (Math.abs(start.coords[1] - stop.coords[1]) > 10) {
+                        event.preventDefault();
+                    }
+                }
+ 
+                $this
+                    .bind(touchMoveEvent, moveHandler)
+                    .one(touchStopEvent, function (event) {
+                        $this.unbind(touchMoveEvent, moveHandler);
+                        if (start && stop) {
+                            if (stop.time - start.time < 1000 &&
+                                Math.abs(start.coords[1] - stop.coords[1]) > 30 &&
+                                Math.abs(start.coords[0] - stop.coords[0]) < 75) {
+                                start.origin
+                                    .trigger("swipeupdown")
+                                    .trigger(start.coords[1] > stop.coords[1] ? "swipeup" : "swipedown");
+                            }
+                        }
+                        start = stop = undefined;
+                    });
+            });
+        }
+    };
+ 
+//Adds the events to the jQuery events special collection
+    $.each({
+        swipedown: "swipeupdown",
+        swipeup: "swipeupdown"
+    }, function (event, sourceEvent) {
+        $.event.special[event] = {
+            setup: function () {
+                $(this).bind(sourceEvent, $.noop);
+            }
+        };
+    });
+ 
+})();
